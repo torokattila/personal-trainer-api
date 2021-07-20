2f35524fab6e87c6dc040e49f62235a3b62fb50b
torokattila <torcsiattila93@gmail.com>
2021-07-20 23:04:45 +0200

    feat(index): user stored in database

--- index.js
+++ index.js
@@ -17,24 +17,73 @@ app.use(
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.DB_USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    insecureAuth: true,
-    multipleStatements: true
+	host: process.env.HOST,
+	user: process.env.DB_USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+	insecureAuth: true,
+	multipleStatements: true
 });
 
 setInterval(function() {
-    db.query("SELECT 1");
+	db.query("SELECT 1");
 });
 
 db.query("USE " + db.database);
 
+const saltRounds = 10;
+
 app.post("/api/registeruser", (req, res) => {
-    console.log(req.body);
+	const { username, password, confirmPassword } = req.body;
+
+	if (password !== confirmPassword) {
+		res.json({
+			error: "The two password fields does not match, please try again!"
+		});
+	} else {
+		const findUserQuery = "SELECT username FROM user WHERE username = ?;";
+
+		db.query(findUserQuery, username, (error, result) => {
+			if (error) {
+				console.log(error);
+				res.json({ error: error });
+			} else if (result.length > 0) {
+				res.json({ error: "This username is already exist!" });
+			} else {
+				const insertUserQuery =
+					"INSERT INTO user SET username = ?, password = ?;";
+
+				bcrypt.hash(
+					password,
+					saltRounds,
+					(hashError, hashedPassword) => {
+						if (hashError) {
+							console.log(hashError);
+							res.json({ error: hashError });
+						}
+
+						db.query(
+							insertUserQuery,
+							[username, hashedPassword],
+							(insertError, insertResult) => {
+								if (insertError) {
+									console.log(insertError);
+									res.json({ error: insertError });
+								} else if (insertResult) {
+									res.json({
+										username: username,
+										userId: insertResult.insertId
+									});
+								}
+							}
+						);
+					}
+				);
+			}
+		});
+	}
 });
 
 app.listen(PORT, () => {
-    console.log(`App is listening on PORT ${PORT}`);
+	console.log(`App is listening on PORT ${PORT}`);
 });
